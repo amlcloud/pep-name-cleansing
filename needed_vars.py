f102c2c0c261636e6f0f486d4fa76a98489cd314
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-01-30 01:43:58 +1100

    Reset needed_vars.py file

--- needed_vars.py
+++ needed_vars.py
@@ -2,6 +2,6 @@
 # you want to save files in (should be a folder)
 import openai
 
-openai.api_key = "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"
-path = "C:/Users/kilik/Desktop/AML/SAVED_FILES"
-repo_path = "C:/Users/kilik/Desktop/AML/pep-name-cleansing"
+openai.api_key = "insert api key here"
+path = "insert path for saved files here"
+repo_path = "insert local path of repo here"
