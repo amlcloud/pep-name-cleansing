cb7bb98d42f92e9f11e1ffa14c445d7173f2daf8
raja-felipe <101044075+raja-felipe@users.noreply.github.com>
2023-01-30 01:57:42 +1100

    Delete needed_vars.py

--- needed_vars.py
+++
@@ -1,7 +0,0 @@
-# put your openai api key and the path
-# you want to save files in (should be a folder)
-import openai
-
-openai.api_key = "insert api key here"
-path = "insert saved files path here"
-repo_path = "insert actual repo path here"
