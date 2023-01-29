b4a7bf174efe803ff75200d45492ad0b71634ef5
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-01-30 02:07:14 +1100

    fixing gitignore

--- needed_vars.py
+++
@@ -1,7 +0,0 @@
-# put your openai api key and the path
-# you want to save files in (should be a folder)
-import openai
-
-openai.api_key = "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"
-path = "C:/Users/kilik/Desktop/AML/SAVED_FILES"
-repo_path = "C:/Users/kilik/Desktop/AML/pep-name-cleansing"
