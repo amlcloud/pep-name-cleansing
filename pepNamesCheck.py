5e50a56c390e27a5e6c2d6a820148fe2cd02b142
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-02-03 11:34:39 +1100

    Updated name accuracy files, added ntbk

--- pepNamesCheck.py
+++ pepNamesCheck.py
@@ -7,12 +7,17 @@ from replace_dict import word_ref
 import re
 from time import sleep
 
+# NAME FIX: Take one name
+# BATCH FIX: Supply list of names (csv)/array -> csv file/array, String -> String, array of json objects -> array of json objects
+# JSON object : { in, out }
+
 class PepNamesCheck:
     def __init__(self):
         self.namesGen = PepOpenAi()
         # CSV Format of the Names Data, including the link they were taken from
         # self.namesData = ["Name;Chosen Name;OpenAi Possible Names;URL"]
-        self.namesData = ["Name;Chosen Name;OpenAi Possible Names;URL;GPT Chosen Name"]
+        # self.namesData = ["Name;Chosen Name;OpenAi Possible Names;URL;GPT Chosen Name"]
+        self.namesData = ['Name;Chosen Name;URL']
         self.logger = logging.getLogger('ftpuploader')
         self.urls = []
         # Extract all the possible names included in the repository
@@ -32,89 +37,32 @@ class PepNamesCheck:
         for name in self.namesGen.names:
 
             # Change the name immediately
-            changedName = self.changeWords(name)
+            # changedName = self.changeWords(name)
 
-            try:
-                # This helper function checks if the immediately proceeding
-                # word in the string is also in the possibleNames attribute
-                # if so, returns the first and last name
-                def makeLastName(index, possible_name, text):
-                    try:
-                        currLast = text.split()[index+1]
-                        # if currLast in last_names:
-                        # print("Testing: "+possible_name+' '+currLast)
-                        # print("In last name: "+str(currLast in self.possibleNames))
-                        # print("In first name: "+str(currLast in self.possibleNames))
-                        if currLast in self.possibleNames:
-                            # print("Returned: "+possible_name+' '+currLast)
-                            return possible_name+' '+currLast
-                        else:
-                            return None
-                    except:
-                        return None
-
-                allNames = []
-                currName = ""
-                addName = ""
-                # Run through each word in the name
-                # to check for possible first and last name combinations
-                for i in range(len(changedName.split())):
-                    poss_name = changedName.split()[i]
-                    # if (poss_name in first_names):
-                    if poss_name in self.possibleNames:
-                        # print("Currently Testing this Name: "+poss_name)
-                        currName = makeLastName(i, poss_name, changedName)
-                    if currName != None:
-                        # print("Appending this name: "+currName)
-                        allNames.append(currName)
-
-                # print(f'Current All Names: {allNames}')
-
-                # If there were possible candidates,
-                # we can take the name that occurred the latest;
-                # assumes that preceeding combinations included titles (Mr, Ms, Hon., etc.)
-                if allNames:
-                    addName = allNames[len(allNames)-1]
-                else:
-                    addName = changedName
-
-                # print(f'Chosen name: {addName}')
-
-                # addData = f'{name};{addName};{str(allNames)};{self.namesGen.urlDict[name]}'
-                # addData = f'{name};{addName};{str(allNames)};{nameUrl}'
-                # addData = f'{name};{addName};{str(allNames)}'
-            
-            # This exception catches when the name's link cannot be found
-            # NOTE: Do not know why yet this is the case, when all of the names'
-            # urls should have been recorded
-            except Exception as e:
-                self.logger.error("Failed to make name for "+name+" due to "+repr(e))
-                # newNames.append(addName)
-                addName = self.changeWords(name)
-                #allNames = []
-                # addData = f'{name};{addName};[];Link not found'
+            # First just insert the name into the database
+            cleansedName = self.namesCleanse(name)
             
             # Here we also test out if chatGPT can just clean the name for us
-            try:
-                gptName = self.GPTchangeWords(name)
-            except Exception as e:
-                self.logger.error(f'Failed to get GPT name because of {repr(e)}')
+            # try:
+            #    gptName = self.GPTchangeWords(name)
+            # except Exception as e:
+            #    self.logger.error(f'Failed to get GPT name because of {repr(e)}')
 
             # In this case, try the same thing for the URL
             try:
-                addData = f'{name};{addName};{str(allNames)};{self.namesGen.urlDict[name]};{gptName}'
+                addData = f'{name};{cleansedName};{self.namesGen.urlDict[name]}'
             except Exception as e:
                 self.logger.error(f'Failed to find the url of {name} due to: {repr(e)}')
-                addData = f'{name};{changedName};[];Link not found;{gptName}'
+                addData = f'{name};{cleansedName};Link not found'
 
             # Record what you added and append it to the new list 
             print(f'Added Name Data: {addData}')
             self.namesData.append(addData)
-            newNames.append(addName)
+            newNames.append(cleansedName)
 
             # To prevent rate limit from reaching
             # ONLY APPLIES FOR GETTING NAMES USING GPT
-            sleep(1)
+            sleep(3)
         
         # Remove duplicates in the list of names present,
         # NOTE: This name generator does not assume names can be from different countries,
@@ -122,6 +70,66 @@ class PepNamesCheck:
         self.namesGen.names = list(set(newNames))
         # self.names = list(set(self.names))
         return
+    
+    # Function that puts together all the name cleaning functins together
+    def namesCleanse(self, name):
+        try:
+            # First use the wordref dictionary to filter
+            # words that would not be part of a name (Mr., Hon., Mrs., etc.)
+            refName = self.changeWords(name)
+
+            # IF the name is at most 2 words, just run through
+            # names dataset to filter the words
+            if len(refName) <= 2:
+                ndName = self.ndChangeWords(refName)
+                return ndName
+            
+            # If it is still longer than 2 words, use GPT to
+            # filter out the words more then run through names dataset
+            else:
+                gptName = self.GPTchangeWords(refName)
+                gptName = self.ndChangeWords(gptName)
+                return gptName
+        
+        except:
+            return name
+    
+    # Helper unction to use the names dataset
+    # to filter out the word
+    def ndChangeWords(self, name):
+        # Local function just to match last names
+        def makeLastName(index, possible_name, text):
+            try:
+                currLast = text.split()[index+1]
+                if currLast in self.possibleNames:
+                    # print("Testing: "+possible_name+' '+currLast)
+                    # print("In last name: "+str(currLast in self.possibleNames))
+                    # print("In first name: "+str(currLast in self.possibleNames))
+                    return f'{possible_name} {currLast}'
+            except:
+                return None
+            return None
+        
+        allNames = []
+        currName = ""
+
+        # Loop through each word and check if word
+        # and next word form a first name last name
+        for i in range(len(name.split())):
+            poss_name = name.split()[i]
+            if poss_name in self.possibleNames:
+                # print("Currently Testing this Name: "+poss_name)
+                currName = makeLastName(i, poss_name, name)
+            if currName != None:
+                # print("Appending this name: "+currName)
+                allNames.append(currName)
+        
+        # Then return the latest occurrence of a name match
+        if allNames:
+            return allNames[len(allNames)-1]
+        else:
+            return name
+
     
     # Function to help filter out filler words in name
     def changeWords(self, name):
@@ -173,3 +181,29 @@ class PepNamesCheck:
         print(f'total count in csv: {len(inCsv)}')
         print(f'total count of all URLS: {len(self.urls)}')
         return
+    
+    # Helper Functions to get Data from the Cleaned Names CSV
+
+    def getCsvDf(self, names_path):
+        df = pd.read_csv(names_path)
+        return df
+
+    def getNamesRetained(self, names_path):
+        df = self.getCsvDf(names_path=names_path)
+        namesList = list(df['Name'])
+        cleanseList = list(df['Chosen Name'])
+        num = 0
+
+        for i in range(len(namesList)):
+            name = namesList[i]
+            cleanse = cleanseList[i]
+
+            if name == cleanse:
+                # print("Name: "+name)
+                num = num + 1
+        
+        # print(f'Total Number of Names: {len(namesList)}')
+        # print(f'Names Kept: {num}')
+        # print(f'Percent Kept: {num/len(namesList)}.2f')
+
+        return df[df['Name'] == df['Chosen Name']]
